fc0103ca3fc45573dcf6b5b7cda42f86456afc8d
Analimalu <124834271+Analimalu@users.noreply.github.com>
2023-11-21 11:46:37 -0300

    Add files via upload

---
+++ trocaimg/estudo.js
@@ -0,0 +1,9 @@
+function cor1 (){
+    document.getElementById("trocaimg").src="trocaimg01.jpg";
+}
+function cor2 (){
+    document.getElementById("trocaimg").src="trocaimg02.jpg";
+}
+function cor3 (){
+    document.getElementById("trocaimg").src="trocaimg03.jpg";
+}
